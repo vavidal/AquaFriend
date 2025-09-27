fa12cd324a418c0e92fb0116fd699e9f9026809a
yazmgou <jzmnmoraga@gmail.com>
2025-09-26 22:43:44 -0300

    página editada

--- Aquafriend/src/app/app.ts
+++ Aquafriend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from "./components/footer/footer";
+import { Main } from "./components/main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [Header, Footer, Main],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
