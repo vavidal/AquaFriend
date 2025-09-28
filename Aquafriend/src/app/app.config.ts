205e34c624a6ec39d4c7b5751b51103111f09700
yazmgou <jzmnmoraga@gmail.com>
2025-09-27 23:50:05 -0300

    formulario funcional

--- Aquafriend/src/app/app.config.ts
+++ Aquafriend/src/app/app.config.ts
@@ -1,15 +1,19 @@
+// src/app/app.config.ts
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+
+// 👇 IMPORTA withFetch
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(),
+    // 👇 usa fetch para HttpClient (mejor para SSR/Vite)
+    provideHttpClient(withFetch()),
     importProvidersFrom(ReactiveFormsModule),
   ],
 };
