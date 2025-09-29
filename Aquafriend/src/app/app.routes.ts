89875fcb4fa7ece495b8c2c6a77d00de85034c56
vavidal <va.vidala@duocuc.cl>
2025-09-29 15:54:55 -0300

    enrutamiento y cambios en boton

--- Aquafriend/src/app/app.routes.ts
+++ Aquafriend/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 
+import { View360Component } from './components/view360/view360';
+
 export const routes: Routes = [
+
+  { path: 'view360', component: View360Component }
 ];
+
