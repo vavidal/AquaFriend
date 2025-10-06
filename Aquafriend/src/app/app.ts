4674c5eb3e9b967fad3e4508477cc49b3e28dfdf
yazmgou <jzmnmoraga@gmail.com>
2025-10-05 21:21:13 -0300

    subir formulario reservas, arreglar ubicación y tamaño de los forms

--- Aquafriend/src/app/app.ts
+++ Aquafriend/src/app/app.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
-import { MainComponent } from './components/main/main';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, Footer, MainComponent, RouterOutlet],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
