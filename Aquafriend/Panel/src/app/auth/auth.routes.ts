7cd57f3733008bd2952fac5c7a076fe988b21e09
yazmgou <jzmnmoraga@gmail.com>
2025-10-09 23:19:59 -0300

    versión anterior

--- Aquafriend/Panel/src/app/auth/auth.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { Login } from './login/login';
-
-export const authRoutes: Routes = [
-  { path: 'login', component: Login, title: 'Login' },
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
-];
--- Aquafriend/Panel/src/app/components/anfibios/anfibios.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { Anfibios } from './anfibios';
-
-export const anfibiosRoutes: Routes = [
-  { path: '', component: Anfibios, title: 'Anfibios' },
-];
