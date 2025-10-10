38929201168745e59355c5e3027eedd0e7d5f48d
yazmgou <jzmnmoraga@gmail.com>
2025-10-09 23:19:59 -0300

    versión anterior

--- Aquafriend/Panel/src/app/components/admin/admin.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const adminRoutes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./admins/admins').then(m => m.Admins),
-    title: 'Administración',
-  },
-];
