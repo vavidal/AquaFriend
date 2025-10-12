7e245b48ab9b15471812d1c89d2f6eebbf319de4
yazmgou <jzmnmoraga@gmail.com>
2025-10-12 16:02:52 -0300

    recorrido virtual vista

---
+++ Aquafriend/src/app/view360/view360.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-view360',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './view360.html',
+  styleUrls: ['./view360.css']
+})
+export class View360Component {}
