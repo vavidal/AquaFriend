c60f8357a9e72c7489dd391a65fc44bc34a15ea3
jazmgou <jzmnmoraga@gmail.com>
2025-10-13 16:00:57 -0300

    CRUD para el apartado usuarios (hasta el momento solo está admin)

--- Aquafriend/src/app/admin/features/users/user.routes.ts
+++ Aquafriend/src/app/admin/features/users/user.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const userRoutes: Routes = [
-  { path: '', redirectTo: 'crear', pathMatch: 'full' },
+  { path: '', redirectTo: 'lista', pathMatch: 'full' },
+  {
+    path: 'lista',
+    loadComponent: () =>
+      import('./users-list.component').then(m => m.UsersListComponent),
+    title: 'Gestión de Usuarios'
+  },
   {
     path: 'crear',
     loadComponent: () =>
-      import('./create-user.component').then(m => m.CreateAccountComponent), 
+      import('./create-user.component').then(m => m.CreateAccountComponent),
     title: 'Crear usuario'
   }
 ];
