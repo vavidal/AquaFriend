233db017aec7553c436b5457352b5b6cc0067e98
vavidal <va.vidala@duocuc.cl>
2025-11-01 00:51:02 -0300

    ajustes

--- Aquafriend/src/app/admin/features/animales/animales.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { Animales } from './animales';
-
-export const animalesRoutes: Routes = [
-  { path: '', component: Animales, title: 'Animales' },
-];
