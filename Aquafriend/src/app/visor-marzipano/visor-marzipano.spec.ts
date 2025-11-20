e018a50fae440233ccc441d6d3df0992c4c73854
vavidal <va.vidala@duocuc.cl>
2025-11-20 16:28:25 -0300

    correccion sintaxis

--- Aquafriend/src/app/visor-marzipano/visor-marzipano.spec.ts
+++ Aquafriend/src/app/visor-marzipano/visor-marzipano.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { VisorMarzipano } from './visor-marzipano';
+import { VisorMarzipanoComponent } from './visor-marzipano';
 
 describe('VisorMarzipano', () => {
-  let component: VisorMarzipano;
-  let fixture: ComponentFixture<VisorMarzipano>;
+  let component: VisorMarzipanoComponent;
+  let fixture: ComponentFixture<VisorMarzipanoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [VisorMarzipano]
+      imports: [VisorMarzipanoComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(VisorMarzipano);
+    fixture = TestBed.createComponent(VisorMarzipanoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
